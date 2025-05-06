419d13befe52724e26f6a0b2004ea65f9423e009
Cameron Abo <cabo0@csu.fullerton.edu>
2025-05-05 17:50:15 -0700

    Barebones but fully Functional

--- app/src/main/java/com/example/blackjacksefinalproject/MainActivity.java
+++ app/src/main/java/com/example/blackjacksefinalproject/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
